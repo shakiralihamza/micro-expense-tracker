4d759cba5545899c3566aa249aaeddc8ccae3010
shaki <shakiralihamza05@gmail.com>
2021-10-13 13:00:21 +0500

    Removed major things

--- src/components/MeetingsMenu.js
+++ src/components/MeetingsMenu.js
@@ -3,78 +3,90 @@ import {makeStyles} from "@material-ui/core/styles";
 import MeetingsMenuContext from "../context/MeetingsMenuContext";
 import AppBar from '@material-ui/core/AppBar';
 import Box from "@material-ui/core/Box";
-import {CssBaseline, Grid, Link} from "@material-ui/core";
+import {CssBaseline, Grid, Input, InputAdornment, InputLabel, Link, OutlinedInput, TextField} from "@material-ui/core";
 import AddIcon from '@material-ui/icons/Add';
-import FilterListIcon from '@material-ui/icons/FilterList';
+import {number} from "prop-types";
+import FormControl from "@material-ui/core/FormControl";
+import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         fontSize: 15
     },
     AppBarStyle: {
+        marginTop: '15px',
         backgroundColor: 'rgba(255,255,255,0.92)',
         boxShadow: 'none',
-    },
-    GridStyle: {
-        padding: '4px 0'
-    },
-    menuGrid: {
-    padding: '5px 20px',
-    // backgroundColor: 'aqua'
-}
-
+        color: "black"
+    }
 }));
 
 
 const MeetingsMenu = (props) => {
     const something = React.useContext(MeetingsMenuContext);
 
-    const handleClickOpen = () => {
-        something.state.setOpenAddMeeting(true);
-    };
-
 
 
     const classes = useStyles();
+    const [values, setValues] = React.useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({...values, [prop]: event.target.value});
+    };
 
 
     return (
         <>
-            <CssBaseline />
-                <AppBar className={classes.AppBarStyle}>
-                    <Box className={classes.root}>
+            <CssBaseline/>
+            <AppBar className={classes.AppBarStyle}>
+                <Box className={classes.root}>
+                    <Container maxWidth={"md"}>
                         <Grid
-                            className={classes.GridStyle}
                             container
                             direction={'row'}
-                            justify="space-around"
+                            justify="space-evenly"
                             alignItems="center"
                         >
-                            <Grid item className={classes.menuGrid}>
-                                <Link
-                                    style={{margin: 'auto', textAlign:'center'}}
-                                    component="button"
-                                    // variant="caption"
-                                    underline={'none'}
-                                >
-                                    <FilterListIcon fontSize='medium'/>
-                                </Link>
+                            <Grid item>
+                                <TextField
+                                    fullWidth
+                                    label="Title"
+                                    size={"small"}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    variant="outlined"
+                                />
                             </Grid>
                             <Grid item>
-                                Expenses
+                                <FormControl size={"small"} fullWidth variant="outlined">
+                                    <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                    <OutlinedInput
+                                        id="outlined-adornment-amount"
+                                        value={values.amount}
+                                        onChange={handleChange('amount')}
+                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                        labelWidth={60}
+                                    />
+                                </FormControl>
                             </Grid>
-                            <Grid item className={classes.menuGrid} onClick={handleClickOpen}>
-                                Add<Link
-                                    component="button"
-                                    underline={'none'}
-                                >
-                                    <AddIcon fontSize={'medium'}/>
-                                </Link>
+                            <Grid item>
+                                <IconButton color={"primary"}>
+                                    <AddIcon />
+                                </IconButton>
                             </Grid>
                         </Grid>
-                    </Box>
-                </AppBar>
+                    </Container>
+                </Box>
+            </AppBar>
         </>
     );
 };
